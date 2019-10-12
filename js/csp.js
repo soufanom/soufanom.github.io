0a0ebc9f4dcdec0b2358032e56a5971f76198ac3
soufanom <othman.soufan@gmail.com>
2019-10-12 05:36:48 +0300

    Update csp.js

--- js/csp.js
+++ js/csp.js
@@ -1,6 +1,17 @@
-function traverseNet(s) {
-  console.log(s);
-  s.graph.nodes().forEach(function (n) {
-    console.log(n);
+function traverseNet(sigInst) {
+  $.getJSON('data.json', function (data) {
+    var nodes = data['nodes'];
+    var edges = data['edges'];
+    for (var i = 0; i < nodes.length; i++) {
+      sigInst.graph.addNode(nodes[i]);
+    }
+    for (var j = 0; j < edges.length; j++) {
+      sigInst.graph.addEdge(edges[j]);
+    }
+    sigInst.refresh();
+    sigInst.graph.nodes().forEach(function (n) {
+      console.log(n);
+    });    
   });
+  
 }
